b693ff3ee3b15a76379cd37df40f1ddcab64b93c
Jonathan Rodriguez <launchx06724@innovaccion.mx>
2022-04-18 20:38:38 -0500

    edit: final version

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -2,6 +2,6 @@ const MissionCommander = require('./../app/missionCommander');
 describe("Unit Tests for Mission Commander Class", () => {
     test('1) Create a mission commander objet', () => {
         const myMissionCommander = new MissionCommander("Woopa")
-        expect(myMissionCommander.name).toBe("Woopo");
+        expect(myMissionCommander.name).toBe("Woopa");
     });
 })
